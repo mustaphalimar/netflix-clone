548fa46c1f8a44ca3b01167ca06e74aec579b17d
mustaphalimar <tapham079@gmail.com>
2023-12-27 14:24:46 +0100

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "netflix-clone-mus.firebaseapp.com",
   projectId: "netflix-clone-mus",
   storageBucket: "netflix-clone-mus.appspot.com",
-  messagingSenderId: "20240355269",
-  appId: "1:20240355269:web:9b043a597387f980ca45ac",
-  measurementId: "G-MVHBPNTRNV",
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
